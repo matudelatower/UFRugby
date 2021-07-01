8548522b25e18667e00c3986165c24d725783d25
Matias Solis de la Torre <matias.delatorre89@gmail.com>
2021-07-01 12:18:59 -0300

    fix user list. fix logo

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -9,153 +9,182 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("/usuario")
  */
-class UsuarioController extends AbstractController
-{
-    /**
-     * @Route("/", name="usuario_index", methods={"GET"})
-     */
-    public function index(Request $request, PaginatorInterface $paginator): Response
-    {
+class UsuarioController extends AbstractController {
+
+	private $passwordEncoder;
+	private $mailer;
+
+	public function __construct( UserPasswordEncoderInterface $passwordEncoder, MailerInterface $mailer ) {
+		$this->passwordEncoder = $passwordEncoder;
+		$this->mailer          = $mailer;
+	}
+
+	/**
+	 * @Route("/", name="usuario_index", methods={"GET"})
+	 */
+	public function index( Request $request, PaginatorInterface $paginator ): Response {
 
 
-        $filterType = $this->createForm(UsuarioFilterType::class,
-            null,
-            [
-                'method' => 'GET'
-            ]);
+		$filterType = $this->createForm( UsuarioFilterType::class,
+			null,
+			[
+				'method' => 'GET'
+			] );
 
-        $filterType->handleRequest($request);
+		$filterType->handleRequest( $request );
 
-        if ($filterType->get('buscar')->isClicked()) {
-            $usuarios = $this->getDoctrine()->getRepository(Usuario::class)->findQbBuscar($filterType->getData());
-        } else {
-            $usuarios = $this->getDoctrine()
-                ->getRepository(Usuario::class)
-                ->findQbAll();
-        }
-
-
-        $usuarios = $paginator->paginate(
-            $usuarios, /* query NOT result */
-            $request->query->getInt('page', 1)/*page number*/,
-            10/*limit per page*/
-        );
-
-        return $this->render('usuario/index.html.twig', [
-            'usuarios' => $usuarios,
-            'filter_type' => $filterType->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="usuario_new", methods={"GET","POST"})
-     */
-    public function new(Request $request, \Swift_Mailer $mailer): Response
-    {
-        $usuario = new Usuario();
-        $usuario->setPlainPassword(substr(md5(uniqid(rand(), true)), 0, 8));
-        $form = $this->createForm(UsuarioType::class, $usuario);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $plainPass = $usuario->getPlainPassword();
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($usuario);
-            $entityManager->flush();
-
-            $this->enviarMailUsuario($usuario->getEmail(), $usuario->getUsername(), $plainPass, $mailer);
-
-            $this->get('session')->getFlashBag()->add('success', 'El usuario se creó con éxito, 
-            se envió un mail al usuario con los datos de ingreso');
+		if ( $filterType->get( 'buscar' )->isClicked() ) {
+			$usuarios = $this->getDoctrine()->getRepository( Usuario::class )->findQbBuscar( $filterType->getData() );
+		} else {
+			$usuarios = $this->getDoctrine()
+			                 ->getRepository( Usuario::class )
+			                 ->findQbAll();
+		}
 
 
-            return $this->redirectToRoute('usuario_index');
-        }
+		$usuarios = $paginator->paginate(
+			$usuarios, /* query NOT result */
+			$request->query->getInt( 'page', 1 )/*page number*/,
+			10/*limit per page*/
+		);
 
-        return $this->render('usuario/new.html.twig', [
-            'usuario' => $usuario,
-            'form' => $form->createView(),
-        ]);
-    }
+		return $this->render( 'usuario/index.html.twig',
+			[
+				'usuarios'    => $usuarios,
+				'filter_type' => $filterType->createView()
+			] );
+	}
 
-    /**
-     * @Route("/{id}", name="usuario_show", methods={"GET"})
-     */
-    public function show(Usuario $usuario): Response
-    {
-        return $this->render('usuario/show.html.twig', [
-            'usuario' => $usuario,
-        ]);
-    }
+	/**
+	 * @Route("/new", name="usuario_new", methods={"GET","POST"})
+	 */
+	public function new( Request $request ): Response {
+		$usuario   = new Usuario();
+		$plainPass = substr( md5( uniqid( rand(), true ) ), 0, 8 );
 
-    /**
-     * @Route("/{id}/edit", name="usuario_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Usuario $usuario): Response
-    {
-        $form = $this->createForm(UsuarioType::class, $usuario);
-        $form->handleRequest($request);
+		$form = $this->createForm( UsuarioType::class, $usuario );
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+		$form->remove( 'plainPassword' );
 
-            return $this->redirectToRoute('usuario_index', [
-                'id' => $usuario->getId(),
-            ]);
-        }
+		$form->handleRequest( $request );
 
-        return $this->render('usuario/edit.html.twig', [
-            'usuario' => $usuario,
-            'form' => $form->createView(),
-        ]);
-    }
+		if ( $form->isSubmitted() && $form->isValid() ) {
+			$usuario->setPassword( $this->passwordEncoder->encodePassword(
+				$usuario,
+				$plainPass
+			) );
+			$entityManager = $this->getDoctrine()->getManager();
+			$entityManager->persist( $usuario );
+			$entityManager->flush();
 
-    /**
-     * @Route("/{id}", name="usuario_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Usuario $usuario): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $usuario->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($usuario);
-            $entityManager->flush();
-        }
+			$this->enviarMailUsuario( $usuario->getEmail(), $usuario->getUsername(), $plainPass );
 
-        return $this->redirectToRoute('usuario_index');
-    }
+			$this->get( 'session' )->getFlashBag()->add( 'success',
+				'El usuario se creó con éxito, 
+            se envió un mail al usuario con los datos de ingreso' );
 
-    public function enviarMailUsuario($mail, $usuario, $password, $mailer)
-    {
-//        $mailer = $this->get('mailer');
 
-        $asunto = getenv('APP_SITE_NAME') . ' - Nuevo Usuario';
+			return $this->redirectToRoute( 'usuario_index' );
+		}
 
-        $url = $this->get('router')->generate('fos_user_security_login', [],
-            UrlGeneratorInterface::ABSOLUTE_URL);
+		return $this->render( 'usuario/new.html.twig',
+			[
+				'usuario' => $usuario,
+				'form'    => $form->createView(),
+			] );
+	}
 
-        // TODO agregar no-reply@urp.org.py cuando se cree la casilla
-        $message = (new \Swift_Message($asunto))
-            ->setFrom(getenv( 'MAILER_USER' ), getenv('APP_UNION_NAME'))
-            ->setTo($mail)
-            ->setBody(
-                $this->renderView(
-                    'emails/nuevo_usuario.html.twig',
-                    [
-                        'nombre' => $usuario,
-                        'password' => $password,
-                        'url' => $url
-                    ]
-                ),
-                'text/html'
-            );
+	/**
+	 * @Route("/{id}", name="usuario_show", methods={"GET"})
+	 */
+	public function show( Usuario $usuario ): Response {
+		return $this->render( 'usuario/show.html.twig',
+			[
+				'usuario' => $usuario,
+			] );
+	}
 
-        $mailer->send($message);
+	/**
+	 * @Route("/{id}/edit", name="usuario_edit", methods={"GET","POST"})
+	 */
+	public function edit( Request $request, Usuario $usuario ): Response {
+		$form = $this->createForm( UsuarioType::class, $usuario );
+		$form->handleRequest( $request );
 
-    }
+		if ( $form->isSubmitted() && $form->isValid() ) {
+
+			$plainPass = $request->request->get( 'usuario' )['plainPassword'];
+
+			$usuario->setPassword( $this->passwordEncoder->encodePassword(
+				$usuario,
+				$plainPass
+			) );
+
+			$this->getDoctrine()->getManager()->flush();
+
+			$this->get( 'session' )->getFlashBag()->add(
+				'success',
+				'Usuario modificado correctamente!'
+			);
+
+			return $this->redirectToRoute( 'usuario_index',
+				[
+					'id' => $usuario->getId(),
+				] );
+		}
+
+		return $this->render( 'usuario/edit.html.twig',
+			[
+				'usuario' => $usuario,
+				'form'    => $form->createView(),
+			] );
+	}
+
+	/**
+	 * @Route("/{id}", name="usuario_delete", methods={"DELETE"})
+	 */
+	public function delete( Request $request, Usuario $usuario ): Response {
+		if ( $this->isCsrfTokenValid( 'delete' . $usuario->getId(), $request->request->get( '_token' ) ) ) {
+			$entityManager = $this->getDoctrine()->getManager();
+			$entityManager->remove( $usuario );
+			$entityManager->flush();
+		}
+
+		return $this->redirectToRoute( 'usuario_index' );
+	}
+
+	public function enviarMailUsuario( $mail, $usuario, $password ) {
+
+		$asunto = $_ENV['APP_SITE_NAME'] . ' - Nuevo Usuario';
+
+		$url = $this->get( 'router' )->generate( 'app_login',
+			[],
+			UrlGeneratorInterface::ABSOLUTE_URL );
+
+		$email = ( new Email() )
+			->from( $_ENV['MAILER_USER'] )
+			->to( $mail )
+			->html(
+				$this->renderView(
+					'emails/nuevo_usuario.html.twig',
+					[
+						'nombre'   => $usuario,
+						'password' => $password,
+						'url'      => $url
+					]
+				)
+			)
+			->subject( $asunto );
+		$this->mailer->send( $email );
+
+	}
 }
--- src/Form/UsuarioType.php
+++ src/Form/UsuarioType.php
@@ -11,48 +11,54 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UsuarioType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('username', TextType::class, [
-                'label' => 'Nombre de usuario'
-            ])
-            ->add('email')
-            ->add('enabled', CheckboxType::class, [
-                'label' => 'Activo'
-            ])
-            ->add('plainPassword', TextType::class, [
-                'label' => 'Password en plano',
-                'required' => false
-            ])
-            ->add('roles', ChoiceType::class, [
-                'multiple' => true,
-                'choices' => [
-                    'ROLE_USER' => 'ROLE_USER',
-                    'ROLE_CLUB' => 'ROLE_CLUB',
-                    'ROLE_REFEREE' => 'ROLE_REFEREE',
-                    'ROLE_REFEREE_ADMIN' => 'ROLE_REFEREE_ADMIN',
-                    'ROLE_UNION' => 'ROLE_UNION'
-                ]
-            ])
+class UsuarioType extends AbstractType {
+	public function buildForm( FormBuilderInterface $builder, array $options ) {
+		$builder
+			->add( 'username',
+				TextType::class,
+				[
+					'label' => 'Nombre de usuario'
+				] )
+			->add( 'email' )
+			->add( 'enabled',
+				CheckboxType::class,
+				[
+					'label' => 'Activo'
+				] )
+			->add( 'plainPassword',
+				TextType::class,
+				[
+					'label'    => 'Password en plano',
+					'required' => false,
+					'mapped'   => false
+				] )
+			->add( 'roles',
+				ChoiceType::class,
+				[
+					'multiple' => true,
+					'choices'  => [
+						'ROLE_USER'          => 'ROLE_USER',
+						'ROLE_CLUB'          => 'ROLE_CLUB',
+						'ROLE_REFEREE'       => 'ROLE_REFEREE',
+						'ROLE_REFEREE_ADMIN' => 'ROLE_REFEREE_ADMIN',
+						'ROLE_UNION'         => 'ROLE_UNION'
+					]
+				] )
 //            ->add('persona')
-            ->add('club',
-                EntityType::class,
-                [
-                    'class' => 'App\Entity\Club',
-                    'required' => false,
-                    'placeholder' => 'Seleccionar Club',
-                    'label' => 'Club *',
-                    'attr' => ['class' => 'select2']
-                ]);
-    }
+			->add( 'club',
+				EntityType::class,
+				[
+					'class'       => 'App\Entity\Club',
+					'required'    => false,
+					'placeholder' => 'Seleccionar Club',
+					'label'       => 'Club *',
+					'attr'        => [ 'class' => 'select2' ]
+				] );
+	}
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => Usuario::class,
-        ]);
-    }
+	public function configureOptions( OptionsResolver $resolver ) {
+		$resolver->setDefaults( [
+			'data_class' => Usuario::class,
+		] );
+	}
 }
